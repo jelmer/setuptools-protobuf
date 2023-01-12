3dfefaa7426bca485b7a15eb9f3b6e98c3929a09
Jelmer Vernooĳ <jelmer+releaser@jelmer.uk>
2023-01-12 15:28:37 +0000

    Release 0.1.5.

--- setuptools_protobuf/__init__.py
+++ setuptools_protobuf/__init__.py
@@ -8,7 +8,7 @@ from setuptools.errors import ExecError, PlatformError  # type: ignore
 import setuptools.command.build
 
 
-__version__ = (0, 1, 4)
+__version__ = (0, 1, 5)
 
 
 def has_protobuf(command):
